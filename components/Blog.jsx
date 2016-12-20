1cf1c4ed199de185ddf336124e6590dfaaf550ab
Amit Shah <amit@amwam.me>
2016-12-20 19:04:31 +0000

    Fix sorting of blog posts in safari

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -12,7 +12,7 @@ export default class Blog extends Component {
         return (
             <div>
                 {posts.filter(post => post.published).sort((a, b) => {
-                    return a.post_number < b.post_number
+                    return b.post_number - a.post_number;
                 }).map(post => (
                     <div key={post.slug} style={{padding: 10}}>
                         <Link to={`/blog/${post.slug}`}>
