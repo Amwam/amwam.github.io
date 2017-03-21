548d97c627e0e145fd6333ca200f366788694e33
Amit Shah <amit@amwam.me>
2017-03-21 09:11:35 +0000

    Upgrade webpack 2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,45 +2,45 @@
 import webpack from 'webpack';
 
 export default {
-    context: __dirname,
-    entry: './index.jsx',
-    output: {
-        path: `${__dirname}/__build__`,
-        filename: 'bundle.js',
-    },
-    module: {
-        loaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
-            {
-                test: /\.json$/,
-                loader: 'json'
-            },
+  context: __dirname,
+  entry: './index.jsx',
+  output: {
+    path: `${__dirname}/__build__`,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
 
-        ],
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
     },
-    resolve: {
-        extensions: ['', '.js', '.jsx'],
- alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat'
+  },
+  plugins: (() => {
+    if (process.argv.indexOf('-p') !== -1) {
+      return [
+        new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
         },
-    },
-    plugins: (() => {
-        if (process.argv.indexOf('-p') !== -1) {
-            return [
-                new webpack.DefinePlugin({
-                    'process.env': {
-                        NODE_ENV: JSON.stringify('production'),
-                    },
-                }),
-                new webpack.optimize.UglifyJsPlugin({
-                    output: {
-                        comments: false,
-                    },
-                }),
-            ];
-        }
-        return [];
-    })(),
-    devtool: (process.argv.indexOf('-p') === -1)? 'eval-source-map' : 'none',
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        output: {
+          comments: false,
+        },
+      }),
+      ];
+    }
+    return [];
+  })(),
+  devtool: (process.argv.indexOf('-p') === -1)? 'eval-source-map' : 'none',
 };
