f8ff379e993a4dc11d1ec685b4d667353141a190
Amit Shah <amit@amwam.me>
2017-05-13 15:52:14 +0100

    Revert back to react, and fix build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -13,34 +13,29 @@ export default {
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
-
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    alias: {
-      'react': 'preact-compat',
-      'react-dom': 'preact-compat'
-    },
   },
   plugins: (() => {
     if (process.argv.indexOf('-p') !== -1) {
       return [
         new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        output: {
-          comments: false,
-        },
-      }),
+          'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+          },
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          output: {
+            comments: false,
+          },
+        }),
       ];
     }
     return [];
   })(),
-  devtool: (process.argv.indexOf('-p') === -1)? 'eval-source-map' : 'none',
+  devtool: process.argv.indexOf('-p') === -1 ? 'eval-source-map' : 'none',
 };
