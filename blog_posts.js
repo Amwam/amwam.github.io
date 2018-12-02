a38447eddff1bd7e00c86eb657cf3d46b12e0df8
Amit Shah <amit@amwam.me>
2018-12-02 20:43:16 +0000

    Added a blog post about switching to typescript

--- blog_posts.js
+++ blog_posts.js
@@ -1,82 +1,90 @@
 export const posts = [
-    {
-        "title": "My First post",
-        "date": "05/07/2014",
-        "post_number": 0,
-        "published": true,
-        "slug": "my-first-post"
-    },
-    {
-        "title": "Branch Per Feature",
-        "date": "12/07/2014",
-        "post_number": 1,
-        "published": false,
-        "tags": ["git"]
-    },
-    {
-        "title": "Alfred: Jenkins workflow",
-        "date": "03/09/2014",
-        "post_number": 2,
-        "published": true,
-        "slug": "alfred-jenkins-workflow",
-        "tags": ["alfred", "jenkins"]
-    },
-    {
-        "title": "My .vimrc",
-        "date": "11/01/2015",
-        "post_number": 3,
-        "published": true,
-        "slug": "my-vimrc",
-        "tags": ["vim"]
-    },
-    {
-        "title": "Update Jenkins workflow to 1.0.1",
-        "date": "20/01/2015",
-        "post_number": 4,
-        "published": true,
-        "slug": "update-jenkins-workflow-1.0.1",
-        "tags": ["alfred", "jenkins"]
-    },
-    {
-        "title": "Using Git push-to-deploy",
-        "date": "06/02/2015",
-        "post_number": 5,
-        "published": true,
-        "slug": "git-push-to-deploy",
-        "tags":["git"]
-    },
-    {
-        "title": "Maintaining a clean Git history",
-        "date": "11/10/2015",
-        "post_number": 7,
-        "published": true,
-        "slug": "maintaining-a-clean-git-history",
-        "tags": ["git"]
-    },
-    {
-        "title": "Update Jenkins workflow to 1.3",
-        "date": "15/05/2016",
-        "post_number": 8,
-        "published": true,
-        "slug": "update-jenkins-wrokflow-1.3",
-        "tags": ["alfred", "jenkins"]
-    },
-    {
-        "title": "Sorting Javascript imports",
-        "date": "17/07/2016",
-        "post_number": 9,
-        "published": true,
-        "slug": "js-import-sort",
-        "tags": ["javascript"]
-    },
-      {
-        "title": "Using React with Web Components",
-        "date": "02/07/2017",
-        "post_number": 10,
-        "published": true,
-        "slug": "react-web-components",
-        "tags": ["javascript", "react"]
-    }
-]
+  {
+    title: "My First post",
+    date: "05/07/2014",
+    post_number: 0,
+    published: true,
+    slug: "my-first-post"
+  },
+  {
+    title: "Branch Per Feature",
+    date: "12/07/2014",
+    post_number: 1,
+    published: false,
+    tags: ["git"]
+  },
+  {
+    title: "Alfred: Jenkins workflow",
+    date: "03/09/2014",
+    post_number: 2,
+    published: true,
+    slug: "alfred-jenkins-workflow",
+    tags: ["alfred", "jenkins"]
+  },
+  {
+    title: "My .vimrc",
+    date: "11/01/2015",
+    post_number: 3,
+    published: true,
+    slug: "my-vimrc",
+    tags: ["vim"]
+  },
+  {
+    title: "Update Jenkins workflow to 1.0.1",
+    date: "20/01/2015",
+    post_number: 4,
+    published: true,
+    slug: "update-jenkins-workflow-1.0.1",
+    tags: ["alfred", "jenkins"]
+  },
+  {
+    title: "Using Git push-to-deploy",
+    date: "06/02/2015",
+    post_number: 5,
+    published: true,
+    slug: "git-push-to-deploy",
+    tags: ["git"]
+  },
+  {
+    title: "Maintaining a clean Git history",
+    date: "11/10/2015",
+    post_number: 7,
+    published: true,
+    slug: "maintaining-a-clean-git-history",
+    tags: ["git"]
+  },
+  {
+    title: "Update Jenkins workflow to 1.3",
+    date: "15/05/2016",
+    post_number: 8,
+    published: true,
+    slug: "update-jenkins-wrokflow-1.3",
+    tags: ["alfred", "jenkins"]
+  },
+  {
+    title: "Sorting Javascript imports",
+    date: "17/07/2016",
+    post_number: 9,
+    published: true,
+    slug: "js-import-sort",
+    tags: ["javascript"]
+  },
+  {
+    title: "Using React with Web Components",
+    date: "02/07/2017",
+    post_number: 10,
+    published: true,
+    slug: "react-web-components",
+    tags: ["javascript", "react"]
+  },
+  {
+    title: "Switching from flow to TypeScript",
+    date: "02/12/2018",
+    post_number: 11,
+    published: true,
+    slug: "flow-to-typesript",
+    tags: ["javascript", "typescript"]
+  }
+];
 
 export default posts;
