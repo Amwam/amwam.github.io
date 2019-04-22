45dbf3c4042e71175059bc3baae65a930a5b1f58
Amit Shah <amit@amwam.me>
2019-04-22 16:59:52 +0100

    Blog post about running React with python

--- blog_posts.js
+++ blog_posts.js
@@ -1,98 +1,106 @@
 export const posts = [
   {
-    title: "My First post",
-    date: "2014-07-05",
+    title: 'My First post',
+    date: '2014-07-05',
     post_number: 0,
     published: true,
-    slug: "my-first-post"
+    slug: 'my-first-post',
   },
   {
-    title: "Branch Per Feature",
-    date: "2014-07-12",
+    title: 'Branch Per Feature',
+    date: '2014-07-12',
     post_number: 1,
     published: false,
-    tags: ["git"]
+    tags: ['git'],
   },
   {
-    title: "Alfred: Jenkins workflow",
-    date: "2014-09-03",
+    title: 'Alfred: Jenkins workflow',
+    date: '2014-09-03',
     post_number: 2,
     published: true,
-    slug: "alfred-jenkins-workflow",
-    tags: ["alfred", "jenkins"]
+    slug: 'alfred-jenkins-workflow',
+    tags: ['alfred', 'jenkins'],
   },
   {
-    title: "My .vimrc",
-    date: "2015-01-11",
+    title: 'My .vimrc',
+    date: '2015-01-11',
     post_number: 3,
     published: true,
-    slug: "my-vimrc",
-    tags: ["vim"]
+    slug: 'my-vimrc',
+    tags: ['vim'],
   },
   {
-    title: "Update Jenkins workflow to 1.0.1",
-    date: "2015-01-20",
+    title: 'Update Jenkins workflow to 1.0.1',
+    date: '2015-01-20',
     post_number: 4,
     published: true,
-    slug: "update-jenkins-workflow-1.0.1",
-    tags: ["alfred", "jenkins"]
+    slug: 'update-jenkins-workflow-1.0.1',
+    tags: ['alfred', 'jenkins'],
   },
   {
-    title: "Using Git push-to-deploy",
-    date: "2015-02-06",
+    title: 'Using Git push-to-deploy',
+    date: '2015-02-06',
     post_number: 5,
     published: true,
-    slug: "git-push-to-deploy",
-    tags: ["git"]
+    slug: 'git-push-to-deploy',
+    tags: ['git'],
   },
   {
-    title: "Maintaining a clean Git history",
-    date: "2015-10-11",
+    title: 'Maintaining a clean Git history',
+    date: '2015-10-11',
     post_number: 7,
     published: true,
-    slug: "maintaining-a-clean-git-history",
-    tags: ["git"]
+    slug: 'maintaining-a-clean-git-history',
+    tags: ['git'],
   },
   {
-    title: "Update Jenkins workflow to 1.3",
-    date: "2016-05-15",
+    title: 'Update Jenkins workflow to 1.3',
+    date: '2016-05-15',
     post_number: 8,
     published: true,
-    slug: "update-jenkins-wrokflow-1.3",
-    tags: ["alfred", "jenkins"]
+    slug: 'update-jenkins-wrokflow-1.3',
+    tags: ['alfred', 'jenkins'],
   },
   {
-    title: "Sorting Javascript imports",
-    date: "2016-07-17",
+    title: 'Sorting Javascript imports',
+    date: '2016-07-17',
     post_number: 9,
     published: true,
-    slug: "js-import-sort",
-    tags: ["javascript"]
+    slug: 'js-import-sort',
+    tags: ['javascript'],
   },
   {
-    title: "Using React with Web Components",
-    date: "2017-07-02",
+    title: 'Using React with Web Components',
+    date: '2017-07-02',
     post_number: 10,
     published: true,
-    slug: "react-web-components",
-    tags: ["javascript", "react"]
+    slug: 'react-web-components',
+    tags: ['javascript', 'react'],
   },
   {
-    title: "Switching from flow to TypeScript",
-    date: "2018-12-02",
+    title: 'Switching from flow to TypeScript',
+    date: '2018-12-02',
     post_number: 11,
     published: true,
-    slug: "flow-to-typesript",
-    tags: ["javascript", "typescript"]
+    slug: 'flow-to-typesript',
+    tags: ['javascript', 'typescript'],
   },
   {
-    title: "Preventing double clicks in React",
-    date: "2019-01-14",
+    title: 'Preventing double clicks in React',
+    date: '2019-01-14',
     post_number: 12,
     published: true,
-    slug: "preventing-double-clicks-in-react",
-    tags: ["javascript", "react"]
-  }
+    slug: 'preventing-double-clicks-in-react',
+    tags: ['javascript', 'react'],
+  },
+  {
+    title: 'React in python (Pyodide)',
+    date: '2019-04-22',
+    post_number: 13,
+    published: true,
+    slug: 'react-in-python',
+    tags: ['javascript', 'react', 'python'],
+  },
 ];
 
 export default posts;
