23a6c3ce07df35f4b6d425a643c5f3e3f2f7f27a
Amit Shah <amit@amwam.me>
2020-05-23 10:12:18 +0100

    Optimise loading of prism

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,12 +19,6 @@ export default function MyApp({ Component, pageProps }) {
           href="https://cdn.jsdelivr.net/npm/prismjs@1.20.0/themes/prism.css"
           rel="stylesheet"
         />
-        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.20.0/prism.min.js"></script>
-        <script
-          src="
-https://cdn.jsdelivr.net/npm/prismjs@1.20.0/plugins/autoloader/prism-autoloader.min.js
-"
-        ></script>
       </Head>
       <Navigation />
       <div id={styles['content-wrapper']}>
