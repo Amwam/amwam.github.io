089f0f3d5216001a4b4798ec8b6fc39bb012fdfe
Amit Shah <amit@amwam.me>
2022-11-26 15:17:40 +0000

    Makei my job title look a bit better

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import styles from './index.module.css';
+import * as React from "react";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
@@ -7,21 +7,24 @@ export default function Home() {
       <h1>Home</h1>
       <div>
         <ul>
-          <li className={styles['link-item']}>
-            Chief Architect EP International @{' '}
-            <a href="https://www.ep.com/">Entertainment Partners UK</a> (Entertainment Partners aquired We Got POP LTD in 2020)
+          <li className={styles["link-item"]}>
+            Chief Architect EP International @{" "}
+            <a href="https://www.ep.com/">Entertainment Partners UK</a>
+            <ul>
+              <li>(Entertainment Partners aquired We Got POP LTD in 2020)</li>
+            </ul>
           </li>
-          <li className={styles['link-item']}>
+          <li className={styles["link-item"]}>
             Mentor @ <a href="https://codeyourfuture.co/">Code Your Future</a>
           </li>
-          <li className={styles['link-item']}>
+          <li className={styles["link-item"]}>
             Committer @ <a href="https://github.com/Amwam/">GitHub</a>
           </li>
-          <li className={styles['link-item']}>
+          <li className={styles["link-item"]}>
             Tweeter @ <a href="https://twitter.com/Amwam/">Twitter</a>
           </li>
-          <li className={styles['link-item']}>
-            Stack Overflow @{' '}
+          <li className={styles["link-item"]}>
+            Stack Overflow @{" "}
             <a href="https://stackoverflow.com/story/amwam">Stack Overflow</a>
           </li>
         </ul>
