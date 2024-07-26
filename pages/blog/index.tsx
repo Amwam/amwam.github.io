be4a2418a7521c1d05fc48774eb27393a5d62a99
Amit Shah <ashah@ep.com>
2024-07-26 09:23:55 +0000

    Prettier

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -45,7 +45,11 @@ export default function Blog(props: {
                   </Link>
                 </strong>{' '}
                 <div>
-                  <em className={styles['post-date']}>{new Date(post.date).toLocaleDateString('en-GB', { dateStyle: 'long' })}</em>
+                  <em className={styles['post-date']}>
+                    {new Date(post.date).toLocaleDateString('en-GB', {
+                      dateStyle: 'long',
+                    })}
+                  </em>
                 </div>
               </div>
             ))}
