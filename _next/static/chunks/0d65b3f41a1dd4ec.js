0cb19876cb51f6e17e2403df6804d75ad07071de
Amwam <Amwam@users.noreply.github.com>
2025-09-04 09:46:55 +0000

    deploy: 09ef05a92fb05006e5d7177e192a6cfe6d4e1168

--- _next/static/chunks/0d65b3f41a1dd4ec.js
+++ _next/static/chunks/0d65b3f41a1dd4ec.js
@@ -3,7 +3,7 @@ __turbopack_load_page_chunks__("/blog/[slug]", [
   "static/chunks/47a270e501e65340.js",
   "static/chunks/181061b33a699bc3.js",
   "static/chunks/28d7fb3b5b7550cd.js",
-  "static/chunks/c71596b7917925eb.js",
+  "static/chunks/8c756fb7a9550c74.js",
   "static/chunks/274988711c50b9b5.css",
-  "static/chunks/turbopack-96325418f171a810.js"
+  "static/chunks/turbopack-83232fe7e9bb369e.js"
 ])
